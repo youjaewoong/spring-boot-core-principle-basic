d7bac1ea8a85caed3f90fb346716a6c0896e25af
gos1004 <nnv711@gmail.com>
2021-11-06 11:02:32 +0900

    새로운 구조와 할인 정책 적용

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,12 +10,22 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-	 public MemberService memberService() {
-		 return new MemberServiceImpl(new MemoryMemberRepository());
-	 }
-	 public OrderService orderService() {
-		 return new OrderServiceImpl(
-				 new MemoryMemberRepository(),
-				 new FixDiscountPolicy());
-	 }
+	
+	public MemberService memberService() {
+		return new MemberServiceImpl(memoryMemberRepository());
+	}
+	public OrderService orderService() {
+		return new OrderServiceImpl(
+				memoryMemberRepository(),
+				discountPolicy());
+	}
+	
+	private MemoryMemberRepository memoryMemberRepository() {
+		return new MemoryMemberRepository();
+	}
+	
+	private DiscountPolicy discountPolicy() {
+		//return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
+	}
 }
